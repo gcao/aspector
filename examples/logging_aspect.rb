b78164e3ed6ddca895047d5b0e042e7074e192c3
Guoliang Cao <gcao99@gmail.com>
2011-12-11 11:10:30 -0500

    Make aspect more readable by defining constants for all methods pattern

--- examples/logging_aspect.rb
+++ examples/logging_aspect.rb
@@ -13,8 +13,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'aspector'
 
 class LoggingAspect < Aspector::Base
+  ALL_METHODS = /.*/
 
-  around /.*/, :except => [:class], :context_arg => true do |context, *args, &block|
+  around ALL_METHODS, :except => :class, :context_arg => true do |context, *args, &block|
     class_method = "#{self.class}.#{context.method_name}"
     puts "Entering #{class_method}: #{args.join(',')}"
     result = block.call *args

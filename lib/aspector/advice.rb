ba48f1b055fde2828526ed8aed34a3fcd7fa91e5
Guoliang Cao <gcao99@gmail.com>
2012-02-13 10:21:53 -0500

    Fix Advice#with_method

--- lib/aspector/advice.rb
+++ lib/aspector/advice.rb
@@ -17,7 +17,7 @@ module Aspector
     end
 
     def with_method
-      @with_method ||= "aspect_#{@parent.hash}_#{@parent.send(:aop_advices).index(self)}"
+      @with_method ||= "aop_#{hash}"
     end
 
     def match? method, context = nil

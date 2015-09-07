d5139cd23aa47e5bed404376ab222053c56dbf8a
Maciej Mensfeld <maciej@mensfeld.pl>
2015-09-07 14:57:38 +0200

    logging part code documentation

--- lib/aspector/logger.rb
+++ lib/aspector/logger.rb
@@ -1,24 +1,39 @@
 module Aspector
-  # Default logger for Aspector
-  # @note It uses ::Logger features - providing basic logging
+  # Default logger for Aspector gem
+  # @note String joining and logging can be resource consuming, so watch out what you log,
+  #   especially when not in debug mode
   class Logger < ::Logger
     attr_reader :context
 
+    # @param context [Aspector::Base, Class] context in which we want to log information
+    # @note it will log more details if it is an Aspector internal context
+    # @note It will try to detect log level based on ASPECTOR_LOG_LEVEL and will fallback to
+    #   ::Logger::ERROR level it if fails
+    # @return [Aspector::Logger] logger instance
     def initialize(context)
       super(STDOUT)
       @context = context
       @level = (ENV['ASPECTOR_LOG_LEVEL'] || ::Logger::ERROR).to_i
     end
 
+    # Sets up all the logging methods
     %i( debug info warn error fatal ).each do |level|
       define_method level do |*args|
-        super(nil) { postfix(*args) }
+        # We pass it as a block, so it won't be evaluated unless we really want to log it
+        # If we would pass it as a method invocation (not as a block), it would evaluate it
+        # even if we would not log it afterwards because the log level doesnt match.
+        super(nil) { message(*args) }
       end
     end
 
     private
 
-    def postfix(*args)
+    # Creates a full messages that we want to log
+    # @param any arguments that we want to log based on
+    # @return [String] message string for logging - provides additional context information
+    # @example Create a message based on a single argument
+    #   message('action taken') #=> 'Aspector::Base | ExampleClass | action taken'
+    def message(*args)
       msg = []
 
       if context.is_a? Aspector::Base
--- lib/aspector/logging.rb
+++ lib/aspector/logging.rb
@@ -1,13 +1,31 @@
 module Aspector
   # Class used as a wrapper to get logging instances
+  # It provides logger detection (from ENV) and creates a logger instance with a proper context
   module Logging
     class << self
+      # @param context [Aspector::Base, Class] context in which we want to log information
+      # @note it will log more details if it is an Aspector internal context
+      # @return [Aspector::Logger, Logger] aspector logger that logs in a given context
+      #   or provided logger
+      # @example Create a logger instance inside Aspector::Base
+      #   @logger ||= Aspector::Logging.get_logger(self)
       def get_logger(context)
         (deconstantize(ENV['ASPECTOR_LOGGER'] || 'Aspector::Logger')).new(context)
       end
 
       private
 
+      # @param [String, Symbol] stringified class/module name that we want to convert
+      #   to a real working class/module instance
+      # @return [Class] class/module that was fetched from its string version
+      # @note If it cannot detect a valid class based on the name,
+      #   it will return an Aspector::Logger instance as a fallback
+      # @example Get a class DummyClass based on its 'DummyClass' string
+      #   deconstantize('DummyClass') #=> 'DummyClass'
+      # @example Get a namespaced class reference
+      #   deconstantize('Users::UserRole') #=> User::UserRole
+      # @example Try to get class that doesn't exist
+      #   deconstantize('NonExistingClass') #=> Aspector::Logger
       def deconstantize(klass_name)
         Object.const_get(klass_name.to_s)
       rescue NameError
